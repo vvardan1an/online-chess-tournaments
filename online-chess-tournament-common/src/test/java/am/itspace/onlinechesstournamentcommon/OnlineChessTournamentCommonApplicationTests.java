e47554b7ac8f36be37a0a0636488ec7afc6150f2
Hayk <haykmanukyan2003@gmail.com>
2022-12-01 00:35:38 +0400

    common module

---
+++ online-chess-tournament-common/src/test/java/am/itspace/onlinechesstournamentcommon/OnlineChessTournamentCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.onlinechesstournamentcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class OnlineChessTournamentCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- src/main/java/am/itspace/onlinechesstournaments/repository/OrganizerRepository.java
+++ src/main/java/am/itspace/onlinechesstournaments/repository/OrganizerRepository.java
@@ -1,7 +1,10 @@
 package am.itspace.onlinechesstournaments.repository;
 
 import am.itspace.onlinechesstournaments.entity.Organizer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface OrganizerRepository extends JpaRepository<Organizer,Integer> {
 }
