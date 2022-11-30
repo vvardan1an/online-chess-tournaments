8e78d59f1eba10450a1555a7cba6b1edd0fe9ba5
Hayk <haykmanukyan2003@gmail.com>
2022-12-01 00:47:18 +0400

    mvc module

---
+++ online-chess-tournament-mvc/src/main/java/am/itspace/onlinechesstournamentmvc/OnlineChessTournamentMvcApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.onlinechesstournamentmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChessTournamentMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineChessTournamentMvcApplication.class, args);
+    }
+
+}
