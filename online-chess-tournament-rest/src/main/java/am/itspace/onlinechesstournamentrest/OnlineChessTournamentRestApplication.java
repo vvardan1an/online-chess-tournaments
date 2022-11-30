5d82c5e45ed28408ae381b2d92c7fde7850b8dcf
Hayk <haykmanukyan2003@gmail.com>
2022-12-01 00:44:34 +0400

    rest module

---
+++ online-chess-tournament-rest/src/main/java/am/itspace/onlinechesstournamentrest/OnlineChessTournamentRestApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.onlinechesstournamentrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChessTournamentRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineChessTournamentRestApplication.class, args);
+    }
+
+}
